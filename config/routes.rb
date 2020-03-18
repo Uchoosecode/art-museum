b42074a08717c2ec630ab83b885c74f62b42e8c7
Uchoosecode <awittyl44@gmail.com>
2020-03-18 00:25:54 -0400

    added resources for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+  resources :comments
+  resources :media
+  resources :categories
+  resources :galleries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Defined root_url for devise.
