6934d98e7e8aca2f82dfde1c298887ce256aa544
Uchoosecode <awittyl44@gmail.com>
2020-03-25 16:07:58 -0400

    name, age and bio attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.where(name: data['name'], age: data['age'], email: data['email']).first
     
       # Uncomment the section below if you want users to be created if they don't exist
       unless user
