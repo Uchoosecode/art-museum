cbfbd98b3dadc6bf35a5502b88543a4fe12af526
Uchoosecode <awittyl44@gmail.com>
2020-03-25 15:50:26 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, :controllers =>{  sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
   
+  resources :galleries 
   resources :comments
+  resources :artworks
   resources :media
   resources :categories
-  resources :galleries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Defined root_url for devise.
