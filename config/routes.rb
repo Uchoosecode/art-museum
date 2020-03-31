4addf152371b25f3fb85fbd55a2c2a3877bf4644
Uchoosecode <awittyl44@gmail.com>
2020-03-31 19:16:26 -0400

    nested comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers =>{  sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
   
   resources :galleries  do
-    resources :artworks
+    resources :artworks do
+      resources :comments
+    end
   end
 
+  resources :artworks
   resources :comments
   resources :media
   resources :categories
