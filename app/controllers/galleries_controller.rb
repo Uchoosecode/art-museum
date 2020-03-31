e1eaedfdbfeb88c7ad31d7525c8cada34780c3f8
Uchoosecode <awittyl44@gmail.com>
2020-03-31 09:35:12 -0400

    completed gallery data

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,46 +1,37 @@
 class GalleriesController < ApplicationController
-before_action :authenticate_user!
+before_action :authenticate_user!, :current_user, :params_id, only: [:show, :edit, :update]
 
     def index
-        @galleries = Gallery.all         
+        @galleries = Gallery.all
     end
     
-    def show
-        @gallery = params_id
+    def show     
     end
 
     def new
-        @gallery = Gallery.new
+        @gallery = Gallery.new   
+        @gallery.categories.build
+        @gallery.media.build
     end
 
     def create
         @gallery = Gallery.create(gallery_params)
-
+            
         if @gallery.valid?
-
             redirect_to gallery_path(@gallery)
         else
             render :new
         end
     end
-
-    def edit
-        @gallery =  params_id    
-    end
-
-    def update
-
-        
-    end
+    
 
 private
 
     def gallery_params
-        params.require(:gallery).permit(:title, :description, :image_url, :user_id, :category_name, :medium, images: [])        
+        params.require(:gallery).permit(:title, :user_id, :category_name, :medium_name)
     end
 
     def params_id
-        Gallery.find(params[:id])
+        @gallery = Gallery.find(params[:id])
     end
 end
-
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,19 +1,26 @@
 class Gallery < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-
-  has_many_attached :images
+  validates :title, presence: true
   
   belongs_to :user
   has_many :artworks
-  has_many :categories
-  has_many :media, through: :categories
+  has_many :categories, through: :artworks
+  has_many :media, through: :artworks
   has_many :comments
 
-  # def category_name=(name)
-  #   self.category = Category.find_or_create_by(name: name)
-  # end
+  def category_name=(name)
+    self.category = Category.find_or_create_by(name: name)
+  end
+    
+  def category_name
+    self.category ? self.category.name : nil
+  end
 
-  # def category_name
-  #    self.category ? self.category.name : nil
-  # end
+  def medium_name=(name)
+    self.medium = Medium.find_or_create_by(name: name)
+  end
+
+  def medium_name
+    self.medium ? self.medium.name : nil
+  end
+
 end
