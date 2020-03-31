d6e0ef082e6c673cc5f87d2ba60e38506114ccf8
Uchoosecode <awittyl44@gmail.com>
2020-03-31 09:36:37 -0400

    user data complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
+  
 
   def self.from_omniauth(access_token)
     data = access_token.info
@@ -11,7 +12,7 @@ class User < ApplicationRecord
     
       # Uncomment the section below if you want users to be created if they don't exist
       unless user
-          user = User.create(name: data['name'],
+          user = User.create(name: data['name'], age: data['age'],
             email: data['email'],
             password: Devise.friendly_token[0,20]
             )
