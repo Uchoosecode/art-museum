48e41ccaca2b25598ba85885ea647458f43704d5
Uchoosecode <awittyl44@gmail.com>
2020-04-06 04:34:50 -0400

    artwork validation

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -7,21 +7,6 @@ class Artwork < ApplicationRecord
     
     has_many :comments
 
-    def category_name=(name)
-        self.category = Category.find_or_create_by(name: name)
-    end
-        
-    def category_name
-        self.category ? self.category.name : nil
-    end
-
-    def medium_name=(name)
-        self.medium = Medium.find_or_create_by(name: name)
-    end
-    
-    def medium_name
-        self.medium ? self.medium.name : nil
-    end
-
+    validates :gallery_id, presence: true
 end
     
