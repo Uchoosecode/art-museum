b2f1ca667d95dbac2da76277d36daf33475a7a42
Uchoosecode <awittyl44@gmail.com>
2020-04-06 04:30:25 -0400

    neat spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users, :controllers =>{  sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
   
+  # Defined root_url for devise.
+  root to: "home#index"
+
   resources :galleries  do
-    resources :artworks do
-      resources :comments
-    end
+    resources :artworks 
+  end
+
+  resources :artworks do
+    resources :comments
   end
   
   resources :media
   resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Defined root_url for devise.
-  root to: "home#index"
-
+  
 end
