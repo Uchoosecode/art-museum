b9e9c61744b328574aed2e8047c54822fbcf3cd2
Uchoosecode <awittyl44@gmail.com>
2020-04-07 10:01:46 -0400

    Date to each comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  devise_for :users, :controllers =>{registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, :controllers =>{sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   
   # Defined root_url for devise.
   root to: "home#index"
