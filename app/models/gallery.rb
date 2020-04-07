a4e0462a229f63d334674753dc9261261dccc895
Uchoosecode <awittyl44@gmail.com>
2020-04-07 11:15:58 -0400

    added long_titles scope

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,12 +1,15 @@
 class Gallery < ApplicationRecord
   validates :title, presence: true
   
+  scope :long_titles, -> { where('LENGTH(title) > 20 ') }
+  
   belongs_to :user
   has_many :artworks
   has_many :categories, through: :artworks
   has_many :media, through: :artworks
   has_many :comments, through: :artworks
-
+  
   # has_one_attached :image, through: :artworks
-
+  
+  # scope :long_titles, -> { where('created_at >= ?', Time.now - 1.week) }
 end
